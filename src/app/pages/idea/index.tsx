fdf77b5ecfa290c1df4c949baed4f6cd3b1e3837
ASUS <longthkgcs18852@fpt.edu.vn>
2022-02-11 22:00:08 +0700

    đemo 2

--- src/app/pages/idea/index.tsx
+++ src/app/pages/idea/index.tsx
@@ -1,5 +1,4 @@
 import { Typography } from "@mui/material";
-import React from "react";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
@@ -13,12 +12,15 @@ import Button from "@mui/material/Button";
 import CommentIcon from "@mui/icons-material/Comment";
 import ThumbUpIcon from "@mui/icons-material/ThumbUp";
 import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+import TextField from '@mui/material/TextField';
+import React, {useState } from "react";
 
 const message = `I don't want to work this place !!!! `;
 
 const options = ["None", "Atria", "Callisto"];
 
 const ITEM_HEIGHT = 48;
+
 
 export function IdeaPage() {
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -29,6 +31,7 @@ export function IdeaPage() {
 	const handleClose = () => {
 		setAnchorEl(null);
 	};
+	//const [commentVisible, setCommentVisible] = useState(false);
 
 	return (
 		<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3 }}>
@@ -92,7 +95,69 @@ export function IdeaPage() {
 							</Button>
 						</Stack>
 					</Grid>
-				</Grid>
+				</Grid>			
+
+				<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3, maxHeight: 90, marginLeft: -20}}>
+					<Paper sx={{ maxWidth: 800, my: 1, mx: "auto", p: 2, border: "1px solid" }}>
+						<Grid container wrap="nowrap" spacing={2}>
+							<Grid item>
+								<Avatar>K</Avatar>
+							</Grid>
+							<TextField style={{ width: 700, marginLeft: 20 }} id="standard-basic" label="Comment" variant="standard" />
+						</Grid>
+					</Paper>
+				</Box>
+					
+				<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3, marginLeft: -20}}>
+					<Paper sx={{ maxWidth: 800, my: 1, mx: "auto", p: 2, border: "1px solid" }}>
+						<Grid container wrap="nowrap" spacing={2}>
+							<Grid item>
+								<Avatar>J</Avatar>
+							</Grid>
+							<Grid item xs>
+								<Typography style={{ fontSize: 20 }}>Big Boo</Typography>
+								<Typography style={{ fontSize: 10 }}>IT Department</Typography>
+							</Grid>
+							<Grid item xs>
+								<Typography style={{ fontSize: 20 }}>Cheer!!</Typography>
+							</Grid>
+						</Grid>
+					</Paper>
+				</Box>
+
+				<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3, marginLeft: -20}}>
+					<Paper sx={{ maxWidth: 800, my: 1, mx: "auto", p: 2, border: "1px solid" }}>
+						<Grid container wrap="nowrap" spacing={2}>
+							<Grid item>
+								<Avatar>T</Avatar>
+							</Grid>
+							<Grid item xs>
+								<Typography style={{ fontSize: 20 }}>Bearer Holland</Typography>
+								<Typography style={{ fontSize: 10 }}>Staff</Typography>
+							</Grid>
+							<Grid item xs>
+								<Typography style={{ fontSize: 20 }}>LoL</Typography>
+							</Grid>
+						</Grid>
+					</Paper>
+				</Box>
+
+				<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3, marginLeft: -20}}>
+					<Paper sx={{ maxWidth: 800, my: 1, mx: "auto", p: 2, border: "1px solid" }}>
+						<Grid container wrap="nowrap" spacing={2}>
+							<Grid item>
+								<Avatar>G</Avatar>
+							</Grid>
+							<Grid item xs>
+								<Typography style={{ fontSize: 20 }}>Jonhny Deep</Typography>
+								<Typography style={{ fontSize: 10 }}>QA Manager</Typography>
+							</Grid>
+							<Grid item xs>
+								<Typography style={{ fontSize: 20 }}>Yahooooo !</Typography>
+							</Grid>
+						</Grid>
+					</Paper>
+				</Box>
 			</Paper>
 		</Box>
 	);
--- src/app/pages/profile/index.tsx
+++ src/app/pages/profile/index.tsx
@@ -35,3 +35,4 @@ export function ProfilePage() {
 		</Box>
 	);
 }
+
