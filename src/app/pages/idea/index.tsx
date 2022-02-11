52ad9dbeeee115d37d6a13b3ce1a690e1ceaf628
ASUS <longthkgcs18852@fpt.edu.vn>
2022-02-11 19:24:29 +0700

    Fontend demo

--- src/app/pages/idea/index.tsx
+++ src/app/pages/idea/index.tsx
@@ -1,6 +1,99 @@
 import { Typography } from "@mui/material";
 import React from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import IconButton from "@mui/material/IconButton";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import CommentIcon from "@mui/icons-material/Comment";
+import ThumbUpIcon from "@mui/icons-material/ThumbUp";
+import ThumbDownIcon from "@mui/icons-material/ThumbDown";
+
+const message = `I don't want to work this place !!!! `;
+
+const options = ["None", "Atria", "Callisto"];
+
+const ITEM_HEIGHT = 48;
 
 export function IdeaPage() {
-	return <Typography paragraph> Hello World</Typography>;
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3 }}>
+			<Paper sx={{ maxWidth: 1000, my: 1, mx: "auto", p: 2, border: "1.5px solid" }}>
+				<Grid container wrap="nowrap" spacing={2}>
+					<Grid item>
+						<Avatar>J</Avatar>
+					</Grid>
+					<Grid item xs>
+						<Typography style={{ fontSize: 20 }}>Jonh Holland</Typography>
+						<Typography style={{ fontSize: 10 }}>IT Department</Typography>
+					</Grid>
+					<div>
+						<IconButton
+							aria-label="more"
+							id="long-button"
+							aria-controls={open ? "long-menu" : undefined}
+							aria-expanded={open ? "true" : undefined}
+							aria-haspopup="true"
+							onClick={handleClick}
+						>
+							<MoreVertIcon />
+						</IconButton>
+						<Menu
+							id="long-menu"
+							MenuListProps={{
+								"aria-labelledby": "long-button"
+							}}
+							anchorEl={anchorEl}
+							open={open}
+							onClose={handleClose}
+							PaperProps={{
+								style: {
+									maxHeight: ITEM_HEIGHT * 4.5,
+									width: "20ch"
+								}
+							}}
+						>
+							{options.map((option) => (
+								<MenuItem key={option} selected={option === "Pyxis"} onClick={handleClose}>
+									{option}
+								</MenuItem>
+							))}
+						</Menu>
+					</div>
+				</Grid>
+				<br />
+				<Grid container wrap="nowrap" spacing={2}>
+					<Grid item xs>
+						<Typography>{message}</Typography>
+					</Grid>
+				</Grid>
+				<br />
+				<Grid container wrap="nowrap" spacing={2}>
+					<Grid item xs>
+						<Stack direction="row" spacing={2}>
+							<Button variant="contained" startIcon={<ThumbUpIcon />}></Button>
+							<Button variant="contained" startIcon={<ThumbDownIcon />}></Button>
+							<Button variant="contained" startIcon={<CommentIcon />}>
+								Comment
+							</Button>
+						</Stack>
+					</Grid>
+				</Grid>
+			</Paper>
+		</Box>
+	);
 }
--- src/app/pages/profile/index.tsx
+++ src/app/pages/profile/index.tsx
@@ -1,5 +1,37 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import Avatar from "@mui/material/Avatar";
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+
+const Item = styled(Paper)(({ theme }) => ({
+	...theme.typography.body2,
+	padding: theme.spacing(1),
+	textAlign: "center",
+	color: theme.palette.text.secondary
+}));
 
 export function ProfilePage() {
-	return <div> Hello World</div>;
+	return (
+		<Box sx={{ flexGrow: 1, overflow: "hidden", px: 3 }}>
+			<Paper sx={{ maxWidth: 500, my: 1, mx: "auto", p: 2 }}>
+				<Grid container wrap="nowrap" spacing={2}>
+					<Grid item>
+						<Button>
+							<Avatar style={{ height: 100, width: 100 }}>J</Avatar>
+						</Button>
+					</Grid>
+					<Grid item xs>
+						<Typography style={{ fontSize: 30 }}>Name:Jonh Holland</Typography>
+						<Typography>Phone: +84 021255458</Typography>
+						<Typography>Role: QA Manager</Typography>
+						<Typography>Description: Like Dog, Play game, etc. </Typography>
+					</Grid>
+				</Grid>
+			</Paper>
+		</Box>
+	);
 }
